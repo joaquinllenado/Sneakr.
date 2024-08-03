a54d05ab9d15d7f3645694892c51d1297e5b6b86
Joaquin Llenado <joaquinllenado@gmail.com>
2024-08-03 13:15:18 -0700

    Added footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,24 @@
+import "../css/Footer.css"
+import ReactLogo from "../images/React-logo.svg"
+import NodeLogo from "../images/Node-logo.svg"
+import ExpressLogo from "../images/Express-logo.svg"
+import JSLogo from "../images/Javascript-logo.svg"
+import HTMLLogo from "../images/HTML-logo.svg"
+import CSSLogo from "../images/CSS-logo.svg"
+
 function Footer() {
     return(
-        <>
-            <h1>Hello from Footer!</h1>
-        </>
+        <div className="footer">
+            <p>Built by Joaquin Llenado</p>
+            <ul className="logos">
+                <li><img src={ReactLogo} alt="React Logo"></img></li>
+                <li><img src={NodeLogo} alt="Node Logo"></img></li>
+                <li><img src={ExpressLogo} alt="Express Logo"></img></li>
+                <li><img src={JSLogo} alt="Javascript Logo"></img></li>
+                <li><img src={HTMLLogo} alt="HTML Logo"></img></li>
+                <li><img src={CSSLogo} alt="CSS Logo"></img></li>
+            </ul>
+        </div>
     )
 }
 
