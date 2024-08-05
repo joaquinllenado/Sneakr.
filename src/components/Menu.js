d5eafd2841e420a4ba4a3e22dc733dfebd8bb4e6
Joaquin Llenado <joaquinllenado@gmail.com>
2024-08-05 13:14:11 -0700

    added Hamburger menu for mobile screen width

---
+++ src/components/Menu.js
@@ -0,0 +1,28 @@
+import hamburger from "../images/Hamburger-Menu.svg"
+import "../css/Menu.css"
+
+function Menu({isOpen}){
+    return(
+        <>
+            <div className="hamburger">
+                <div className="burger burger1" />
+                <div className="burger burger2" />
+                <div className="burger burger3" />
+            </div>
+
+            <style jsx>{`
+                .burger1{
+                    transform: ${isOpen ? 'rotate(45deg)' : 'rotate(0)'};
+                }
+                .burger2{
+                    opacity: ${isOpen ? 0 : 1};
+                }
+                .burger3{
+                    transform: ${isOpen ? 'rotate(-45deg)' : 'rotate(0)'};
+                }
+            `}</style>
+        </>
+    )
+}
+
+export default Menu;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,43 @@
+import { useState } from 'react';
 import '../css/NavBar.css';
+import Menu
+ from './Menu';
 function NavBar(){
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
     return(
         <div className='navbar'>
+            <div className="menu" onClick={toggleMenu}>
+                <Menu isOpen={menuOpen} />
+            </div>
             <div id="branding">
-                <h1>
-                    <span className="letter">S</span>
-                    <span className="letter">n</span>
-                    <span className="letter">e</span>
-                    <span className="letter">a</span>
-                    <span className="letter">k</span>
-                    <span className="letter">r</span>
-                    <span className="letter">.</span>
-                </h1>
+                <a href="/">
+                    <h1>
+                        <span className="letter">S</span>
+                        <span className="letter">n</span>
+                        <span className="letter">e</span>
+                        <span className="letter">a</span>
+                        <span className="letter">k</span>
+                        <span className="letter">r</span>
+                        <span className="letter">.</span>
+                    </h1>
+                </a>
             </div>
             <input id="search-bar" type="text" placeholder="Search for brand, color, etc."></input>
-                <nav>
-                    <ul className="navbar-links">
-                        <li><a href="/news/">News</a></li>
-                        <li><a href="/about/">About</a></li>
-                        <li><a href="/help/">Help</a></li>
-                        <li><a href="/sell/">Sell</a></li>
-                    </ul>
-                </nav>
-                <button className="login button">Login</button>
-                <button className="signup button">Sign Up</button>  
+            <nav>
+                <ul className="navbar-links">
+                    <li><a href="/news/">News</a></li>
+                    <li><a href="/about/">About</a></li>
+                    <li><a href="/help/">Help</a></li>
+                    <li><a href="/sell/">Sell</a></li>
+                </ul>
+            </nav>
+            <button className="login button">Login</button>
+            <button className="signup button">Sign Up</button>  
         </div>
     )
 }
