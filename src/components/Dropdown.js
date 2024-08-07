124aa952cd4d28a61882559ff3bde4fe5a4f5e2b
Joaquin Llenado <joaquinllenado@gmail.com>
2024-08-07 13:39:51 -0700

    fixed banner scaling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,17 +4,11 @@ function Dropdown() {
     return(
         <div className="dropdown">
             <ul id="dropdown-list">
-                <li id="dropdown-item"><a href="/browse/brands/">Brands</a></li>
-                <li id="dropdown-item"><a href="/browse/deals/">Deals</a></li>
-                <li id="dropdown-item"><a href="/browse/new/">New</a></li>
-                <li id="dropdown-item"><a href="/browse/men/">Men</a></li>
-                <li id="dropdown-item"><a href="/browse/women/">Women</a></li>
-                <li id="dropdown-item"><a href="/browse/kids/">Kids</a></li>
-                <li id="dropdown-item"><a href="/browse/sneakers/">Sneakers</a></li>
-                <li id="dropdown-item"><a href="/browse/shoes/">Shoes</a></li>
-                <li id="dropdown-item"><a href="/browse/apparel/">Apparel</a></li>
-                <li id="dropdown-item"><a href="/browse/accessories/">Accessories</a></li>
-                <li id="dropdown-item"><a href="/browse/">More Categories</a></li>
+                <li id="dropdown-item"><a href="/browse/mostpopular/">Most Popular</a></li>
+                <li id="dropdown-item"><a href="/browse/jordan/">Jordan</a></li>
+                <li id="dropdown-item"><a href="/browse/nike/">Nike</a></li>
+                <li id="dropdown-item"><a href="/browse/newbalance/">New Balance</a></li>
+                <li id="dropdown-item"><a href="/browse/adidas/">Adidas</a></li>
             </ul>
         </div>
     )
