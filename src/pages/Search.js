812a0a06d87f9a5aef29ee4011ced8fbd483962b
Joaquin Llenado <joaquinllenado@gmail.com>
2024-09-14 11:23:12 -0700

    Added input validation for search queries

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,18 +2,42 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Row from "../components/Row";
 import Modal from "../components/Modal";
+import searchErrorIcon from "../images/search-error-svgrepo-com.svg";
+import { TailSpin } from "react-loader-spinner";
 
 function Search(){
     const [results, setResults] = useState([]);
     const [selectedShoe, setSelectedShoe] = useState(null);
+    const [searchError, setSearchError] = useState(false);
+    const [loading, setLoading] = useState(true);
     const location = useLocation();
     const query = new URLSearchParams(location.search).get('q');
     
     useEffect(() => {
+        setSearchError(false);
+        setLoading(true);
+
         fetch(`https://sneakr-server.vercel.app/search?q=${query}`)
-            .then(response => response.json())
-            .then(data => setResults(data))
-            .catch(error => console.error('Error:', error))
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Check if data is valid
+                if (Array.isArray(data)) {
+                    setResults(data);
+                } else {
+                    throw new Error("Invalid data format");
+                }
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Search failed:", error);
+                setSearchError(true);
+                setLoading(false);
+            });
 
     }, [location.search]);
 
@@ -25,6 +49,29 @@ function Search(){
         setSelectedShoe(null);
     }
 
+    if (loading) {
+        return (
+            <TailSpin
+                ariaLabel="tail-spin-loading"
+                height="100"
+                width="100"
+                color="cornflowerblue"
+                wrapperClass="loading"
+            />
+        )
+    }
+
+    if(searchError){
+        return(
+            <div className="main">
+                <div className="error">
+                    <img src={searchErrorIcon} alt="Search Error"/>
+                    <p>No results found for "{query}" Please try again.</p>
+                </div>
+            </div>
+        )
+    }
+    
     return(
         <div className="main">
             <h3>Search Results for "{query}":</h3>
