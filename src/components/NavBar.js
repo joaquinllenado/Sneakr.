53cf053708d17b72036eadad80cbe3faa099fa49
Joaquin Llenado <joaquinllenado@gmail.com>
2024-12-23 15:47:37 -0800

    NavBar Rehaul Removed: Page links, login buttons Added: GitHub repo link Modified: Hamburger Menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import '../css/NavBar.css';
-import Menu from './Menu';
 import searchButton from "../images/Search.png"
 import { Link, useNavigate } from 'react-router-dom';
+import GitHub_Logo from '../images/github-mark.svg';
+import { Cross as Hamburger } from 'hamburger-react';
 
 function NavBar(){
     const [menuOpen, setMenuOpen] = useState(false);
     const [searchOpen, setSearchOpen] = useState(false);
     const [query, setQuery] = useState('');
     const navigate = useNavigate();
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen)
-    };
 
     const toggleSearch = () => {
         setSearchOpen(!searchOpen)
@@ -22,61 +19,57 @@ function NavBar(){
         navigate(`/search?q=${query}`);
     };
 
-    useEffect(() => {
-        if (menuOpen) {
-            document.body.classList.add('no-scroll');
-        } else {
-            document.body.classList.remove('no-scroll');
-        }
-    }, [menuOpen])    
-
     return(
         <div className='navbar'>
-            <div className="menu" onClick={toggleMenu} tabIndex="0" aria-expanded={menuOpen} aria-controls="navbar-links">
-                <Menu isOpen={menuOpen} />
-            </div>
-            <div id="branding" onClick={(e) => setQuery('')}>
-                <Link to="/">
-                    <h1> Sneakr.</h1>
-                </Link>
-            </div>
-            <input 
-                id="search-bar" 
-                type="text" 
-                placeholder="Search for brand, color, etc."
-                aria-label="Search for shoes by brand, color, etc."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyDown={(e) => {
-                    if(e.key ==="Enter")
-                        handleSearch();
-                }}
-            />
-            <div id="links-and-buttons">
-                <nav aria-label="Main Navigation">
-                    <ul className="navbar-links">
-                        <li><Link to="/news" onClick={menuOpen ? toggleMenu : ''}>News</Link></li>
-                        <li><Link to="/about" onClick={menuOpen ? toggleMenu : ''}>About</Link></li>
-                        <li><Link to="/help" onClick={menuOpen ? toggleMenu : ''}>Help</Link></li>
-                        <li><Link to="/sell" onClick={menuOpen ? toggleMenu : ''}>Sell</Link></li>
-                    </ul>
-                </nav>
-                <div id="buttons">
-                    <button className="login button">Login</button>
-                    <button className="signup button">Sign Up</button>  
+            <div id='navbar-items'>
+                <div id='hamburger-menu'>
+                    <Hamburger size={35} toggled={menuOpen} toggle={setMenuOpen}/>
+                    {menuOpen && (
+                        <div id="hb-items">
+                            <ul id="dropdown-list-hb">
+                                <li id="dropdown-item-hb"><Link to="/mostpopular">Most Popular</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/jordan">Jordan</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/nike">Nike</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/newbalance">New Balance</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/adidas">Adidas</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/puma">Puma</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/crocs">Crocs</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/converse">Converse</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/hoka">Hoka</Link></li>
+                                <li id="dropdown-item-hb"><Link to="/on">On</Link></li>
+                            </ul>
+                            <a href='https://github.com/joaquinllenado/Sneakr.'>
+                                <img id='github-hb' src={GitHub_Logo} />
+                            </a>
+                        </div>
+                    )}
+                </div>
+                <div id="branding" onClick={() => setQuery('')}>
+                    <Link to="/">
+                        <h1> Sneakr.</h1>
+                    </Link>
+                </div>
+                <input 
+                    id="search-bar" 
+                    type="text" 
+                    placeholder="Search for brand, color, etc."
+                    aria-label="Search for shoes by brand, color, etc."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    onKeyDown={(e) => {
+                        if(e.key ==="Enter")
+                            handleSearch();
+                    }}
+                />
+                <a href='https://github.com/joaquinllenado/Sneakr.'>
+                    <img id='github' src={GitHub_Logo} />
+                </a>
+                <div className="search-bar-small" onClick={toggleSearch}>
+                    <img src={searchButton} alt="Open search input"/>
                 </div>
             </div>
-
-            <div className="search-bar-small" onClick={toggleSearch}>
-                <img src={searchButton} alt="Open search input"/>
-            </div>
-
             <style jsx>{`
-                @media (max-width: 767px){
-                    #links-and-buttons{
-                        display: ${menuOpen ? "flex" : "none"};
-                    }
-                    
+                @media (max-width: 767px){                    
                     #search-bar{
                         display: ${searchOpen ? "flex" : "none"};
                     }
